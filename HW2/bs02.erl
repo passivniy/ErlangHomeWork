8c5d0abaf674935c91b2ec6643dc9b89c3dcdd60
Daniil <89728063+passivniy@users.noreply.github.com>
2023-08-08 19:44:39 +0200

    Delete HW2 directory

--- HW2/bs02.erl
+++
@@ -1,8 +0,0 @@
--module(bs02).
--export([words/1]).
-
-words(Acc)-> lists:reverse(words(Acc,[],<<>>)).
-
-words(<<" ",Rest/binary>>,Acc,L)-> words(<<Rest/binary>>,[L|Acc],<<>>);
-words(<<H/utf8,Rest/binary>>,Acc,L)-> words(<<Rest/binary>>,Acc,<<L/binary,H/utf8>>);
-words(<<>>,Acc,L)->[L|Acc].
