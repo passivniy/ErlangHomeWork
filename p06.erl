4f089768d3bd526d9bf769b1cb4555e358fe472c
Daniil <89728063+passivniy@users.noreply.github.com>
2023-08-10 18:41:08 +0200

    Delete p06.erl

--- p06.erl
+++
@@ -1,9 +0,0 @@
--module(p06).
--export([is_palindrome/1]).
-
-is_palindrome(L) -> L == is_palindrome(L, []).
-
-is_palindrome([], L) ->
-  L;
-is_palindrome([H | T], L) ->
-  is_palindrome(T, [H | L]).
